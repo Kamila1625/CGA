9d7f5973fa999266a4f76f7ba191560f691bbe23
Kamila1625 <kamila.de.bua.tresy@gmail.com>
2017-02-21 21:35:54 +0300

    second version

---
+++ Assets/NewJumpScript.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewJumpScript : MonoBehaviour {
+
+    int upFlag = 1;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (transform.position.y > 13)
+        {
+            upFlag = -1;
+        }
+        if (transform.position.y < -3)
+        {
+            upFlag = 1;
+        }
+        Vector3 moveDirection = new Vector3(0, 10 * upFlag * Time.deltaTime, 0);
+        transform.Translate(moveDirection);
+       
+
+    }
+}
---
+++ Assets/NewRingScript.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewRingScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.RotateAround(new Vector3(5, 5, 5), new Vector3(0, 1, 0), 200 * Time.deltaTime);
+    }
+}
